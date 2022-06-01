4171605fd2cc80e471b2c33c72b693593ae706ff
ayazwx <vefaayaz0404@gmail.com>
2022-06-01 23:06:05 +0300

    CSS Udemy Course was finished and CSS Project was created

---
+++ js1.js
@@ -0,0 +1,17 @@
+<html>
+<head>
+<tittle>First My Web Page</tittle>
+</head>
+<body>
+<script>
+    function ChangeText(id) {
+        id.innerHTML = "NewText"
+    }
+    
+    </script>{
+    
+}
+<h1 onclick="ChangeText(this)">Click here </h1>
+
+</body>
+</html>
